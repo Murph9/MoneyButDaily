fc2711707d823b2eb55de85ca2aee079b4d7ca66
Murph9 <murph9y@gmail.com>
2018-08-12 10:18:36 +1000

    Fix entry list formating

--- app/src/main/java/com/murph9/moneybutdaily/RowListAdapter.java
+++ app/src/main/java/com/murph9/moneybutdaily/RowListAdapter.java
@@ -56,11 +56,11 @@ public class RowListAdapter extends RecyclerView.Adapter<RowListAdapter.RowViewH
             final Row r = mRows.get(position);
             holder.rowItem_Category.setText(r.Category);
             holder.rowItem_Amount.setText("$"+r.Amount);
-            holder.rowItem_StartDate.setText(r.From.toString(fromFormat));
-            holder.rowItem_LengthCount.setText(r.LengthCount+"");
+            holder.rowItem_StartDate.setText("@ "+r.From.toString(fromFormat));
+            holder.rowItem_LengthCount.setText(r.LengthCount+" x ");
             holder.rowItem_LengthType.setText(r.LengthType+"");
-            holder.rowItem_IsIncome.setText(r.IsIncome ? "y" : "n");
             holder.rowItem_Repeat.setText(r.RepeatType != DayType.None ? "y" : "n");
+            holder.rowItem_PerDay.setText("$"+H.to2Places(r.CalcPerDay())+"/day");
 
             //add listener for button press
             holder.rowItem_Edit.setOnClickListener(new View.OnClickListener() {
@@ -128,8 +128,8 @@ public class RowListAdapter extends RecyclerView.Adapter<RowListAdapter.RowViewH
         private final TextView rowItem_StartDate;
         private final TextView rowItem_LengthCount;
         private final TextView rowItem_LengthType;
-        private final TextView rowItem_IsIncome;
         private final TextView rowItem_Repeat;
+        private final TextView rowItem_PerDay;
         private final Button rowItem_Edit;
 
         private RowViewHolder(View itemView) {
@@ -139,7 +139,7 @@ public class RowListAdapter extends RecyclerView.Adapter<RowListAdapter.RowViewH
             rowItem_StartDate = itemView.findViewById(R.id.row_list_item_startdate);
             rowItem_LengthCount = itemView.findViewById(R.id.row_list_item_lengthcount);
             rowItem_LengthType = itemView.findViewById(R.id.row_list_item_lengthtype);
-            rowItem_IsIncome = itemView.findViewById(R.id.row_list_item_isincome);
+            rowItem_PerDay = itemView.findViewById(R.id.row_list_item_perday);
             rowItem_Repeat = itemView.findViewById(R.id.row_list_item_repeat);
             rowItem_Edit = itemView.findViewById(R.id.row_list_item_edit);
         }
